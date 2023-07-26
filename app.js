0cd0075f213a2016d67cf51ce0b7eed3dd365891
hashtagteam <hashtag.team@gmail.com>
2023-07-26 22:00:08 +0630

    42_Serving_All_File_Type

--- app.js
+++ app.js
@@ -1,97 +1,44 @@
+require("dotenv").config();
 let http = require("http");
 let url = require("url");
 let fs = require("fs");
 let qs = require("querystring");
-require("dotenv").config();
+let path = require("path");
 
-let responder = (req, res, parm) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(parm);
+meme = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "text/javascript",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".gif": "image/gif",
 };
 
-let myFileReader = (filepath, res) => {
-  fs.access(filepath, fs.F_OK, (err) => {
+let router = (req, res) => {
+  let params = url.parse(req.url, true);
+  let oriPath = params.pathname == "/" ? "/index.html" : params.pathname;
+  let pathname = __dirname + oriPath;
+
+  let ext = path.extname(oriPath);
+  fs.access(pathname, fs.F_OK, (err) => {
     if (err) {
-        res.writeHead(404, {"Content-Type": "test/html"});
-        res.end("<h1>file not found</h1>");
-    }else {
-    fs.readFile(filepath, (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "test/html" });
-      res.end(data);
-    });
-}
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>page not found</h1>");
+    } else {
+      fs.readFile(pathname, (err, data) => {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end("<h1>page not found</h1>");
+        } else {
+          res.writeHead(200, { "Content-Type": meme[ext] });
+          res.end(data);
+        }
+      });
+    }
   });
 };
 
-let routes = {
-  GET: {
-    "/": (req, res) => {
-      let filepath = __dirname + "/index.html";
-      myFileReader(filepath, res);
-    },
-    "/index.html": (req, res) => {
-      let filepath = __dirname + "/index.html";
-      myFileReader(filepath, res);
-    },
-    "/about.html": (req, res) => {
-      let filepath = __dirname + "/about.html";
-      myFileReader(filepath, res);
-    },
-    "/home": (req, res) => {
-      responder(
-        req,
-        res,
-        `<h1> Get Method => /home route params with  ${params.query.name} and ${params.query.age} </h1>`
-      );
-    },
-  },
-  POST: {
-    "/": (req, res) => {
-      responder(req, res, `<h1>Post Method  => / route</h1>`);
-    },
-    "/api/login": (req, res) => {
-      let body = "";
-      req.on("data", (data) => {
-        body += data;
-        if (body.length > 1024) {
-          res.writeHead(403, { "Content-type": "text/html" });
-          res.end("<h1> file size over 1 mb</h1>");
-        }
-      });
-      req.on("end", () => {
-        let query = qs.parse(body);
-        console.log(body);
-        console.log("Email", query.email, "Password", query.password);
-        res.end();
-      });
-    },
-  },
-  NA: (req, res) => {
-    responder(req, res, `<h1>NO Page For That Route!</h1>`);
-  },
-};
-
-let start = (req, res) => {
-  let reqMethod = req.method;
-
-  let params = url.parse(req.url, true);
-  // let name = params.query.name;
-  // let age = params.query.age;
-  //console.log("Name", name, "Age", age);
-
-  let resolveRoute = routes[reqMethod][params.pathname];
-  if (resolveRoute != null && resolveRoute != undefined) {
-    resolveRoute(req, res);
-  } else {
-    routes["NA"](req, res);
-  }
-  //console.log(resolveRoute);
-  //console.log(url_parms);
-  // routes[reqMethod][url]();
-};
-
-let server = http.createServer(start);
+let server = http.createServer(router);
 
 server.listen(process.env.PORT, () => {
   console.log(`Server is running at ${process.env.PORT}`);
