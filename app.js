6cef1a5a33bc52fd3a1908094b18612543c4e824
hashtagteam <hashtag.team@gmail.com>
2023-07-28 21:26:16 +0630

    49_Request_Body_Parameters

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
-let libby = require('./bmlibby/index')
-let abc = process.argv[2]
+require('dotenv').config();
+const bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let app = express();
 
-libby.encode(abc)
-    .then(encoded => libby.compare(abc, encoded))
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
+
+app.get("/api/post/:id", (req, res) => {
+    let id = req.params.id
+    res.send(`this is my ${id}`)
+});
+
+app.get("/api/user", (req,res) => {
+    let name = req.query.name;
+    let age = req.query.age;
+    res.send(`name ${name} age ${age}`);
+})
+
+app.post('/api/login', (req, res) => {
+    let name = req.body.name
+    let age = req.body.age
+
+    res.send(`name ${name} age ${age}`);
+})
+
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + "/index.html")
+// });
+
+// app.get('/index.html', (req, res) => {
+//     res.sendFile(__dirname + "/index.html")
+// });
+
+// app.get('/about.html', (req, res) => {
+//     res.sendFile(__dirname + "/about.html")
+// });
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`server running at ${process.env.PORT}`)
+})
+
