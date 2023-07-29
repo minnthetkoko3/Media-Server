c53244af8b4bfcddada090a3fb3fc4e5acfa91e3
hashtagteam <hashtag.team@gmail.com>
2023-07-29 19:17:54 +0630

    50_Hogan_Template_Engine

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
 require('dotenv').config();
 const bodyParser = require('body-parser');
 let express = require('express');
+let hogan = require('hogan-express');
 let path = require('path');
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.engine('html', hogan)
+app.set('view engine', 'html')
+
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
-app.get("/api/post/:id", (req, res) => {
-    let id = req.params.id
-    res.send(`this is my ${id}`)
-});
 
-app.get("/api/user", (req,res) => {
-    let name = req.query.name;
-    let age = req.query.age;
-    res.send(`name ${name} age ${age}`);
-})
+app.get('/', (req,res) => {
+    res.render('index');
+}); 
 
-app.post('/api/login', (req, res) => {
-    let name = req.body.name
-    let age = req.body.age
+app.get('/about', (req,res) => {
+    res.render('about');
+}); 
 
-    res.send(`name ${name} age ${age}`);
-})
+
+// app.get("/api/post/:id", (req, res) => {
+//     let id = req.params.id
+//     res.send(`this is my ${id}`)
+// });
+
+// app.get("/api/user", (req,res) => {
+//     let name = req.query.name;
+//     let age = req.query.age;
+//     res.send(`name ${name} age ${age}`);
+// })
+
+// app.post('/api/login', (req, res) => {
+//     let name = req.body.name
+//     let age = req.body.age
+
+//     res.send(`name ${name} age ${age}`);
+// })
 
 
 
